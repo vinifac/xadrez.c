171fe8f1fddd66907966a2bf1a0f47b471d4d462
vinifac <tvgordex@gmail.com>
2025-10-11 06:51:56 +0000

    nivel aventureiro

--- xandrex.c
+++ xandrex.c
@@ -8,6 +8,8 @@ int main() {
     int movimentosTorre = 5;
     int movimentosBispo = 5;
     int movimentosRainha = 8;
+    const int movimentosCavaloBaixo = 2; 
+    const int movimentosCavaloEsquerda = 1; 
 
     // Variáveis de controle para os laços while e do-while.
     int contador_bispo = 0;
@@ -21,7 +23,6 @@ int main() {
     for (int i = 0; i < movimentosTorre; i++) {
         printf("Direita\n");
     }
-
     printf("\n----------------------------------------\n\n");
 
     // --- Movimento do Bispo (usando while) ---
@@ -41,7 +42,29 @@ int main() {
         printf("Esquerda\n");
         contador_rainha++; // Incrementa o contador.
     } while (contador_rainha < movimentosRainha);
+    printf("\n----------------------------------------\n\n");
 
+    // ==========================================================
+    //  Implementação lógica nivel aventureiro
+    // ==========================================================
+    
+    printf("--- Movimento do Cavalo ---\n");
+
+    // Loop 'for' externo que executa uma única vez para aninhar os loops de movimento.
+    for (int l_move = 0; l_move < 1; l_move++) {
+        
+        // Loop 'while' INTERNO (aninhado) para simular o movimento de duas casas para baixo.
+        int contador_baixo = 0;
+        while (contador_baixo < movimentosCavaloBaixo) {
+            printf("Baixo\n");
+            contador_baixo++;
+        }
+
+        // Loop 'for' INTERNO (aninhado) para simular o movimento para a esquerda.
+        for(int i = 0; i < movimentosCavaloEsquerda; i++) {
+             printf("Esquerda\n");
+        }
+    }
 
     return 0;
 }
